7b355541a571fc6fd70a3daa3f75aad993f54458
th3l5d <10721924+Th3l5D@users.noreply.github.com>
2020-01-06 02:13:41 +0100

    add exec

--- .aliases/python3.sh
+++ .aliases/python3.sh
@@ -2,7 +2,7 @@
 
 if [ -z $1 ]
 then
-	/usr/bin/python3 -i -c "import colored_traceback.always"
+	exec /usr/bin/python3 -i -c "import colored_traceback.always"
 else
-	/usr/bin/python3 -m colored_traceback $1
+	exec /usr/bin/python3 -m colored_traceback $1
 fi
